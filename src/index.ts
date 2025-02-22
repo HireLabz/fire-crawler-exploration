d3643e54d21b4546ca9560973a2d55e34317549c
Danilo Augusto Correia da Silva <dacs2012@gmail.com>
2025-02-22 14:10:51 -0500

    test worked

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello, TypeScript in Node.js!");
+import FirecrawlApp from "@mendable/firecrawl-js";
+
+const app = new FirecrawlApp({apiKey: process.env.FIRE_CRAWL_API});
+
+const crawlResponse = await app.crawlUrl('https://www.danilo-correia.com/', {
+  limit: 1,
+  scrapeOptions: {
+    formats: ['markdown'],
+  }
+})
+
+// if (!crawlResponse.success) {
+//   throw new Error(`Failed to crawl: ${crawlResponse}`)
+// }
+
+console.log(crawlResponse)
+
